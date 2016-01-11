c861403814badd21ce70081766145c36e7c086f7
Steven Rouk <stevenrouk@gmail.com>
2016-01-11 13:57:23 -0700

    Update app.py with dynamic routes and config['DEBUG']=True

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use decorators to link the function to a url
 @app.route("/")
 @app.route("/hello")
@@ -13,6 +16,19 @@ app = Flask(__name__)
 # define the view using a function, which returns a string
 def hello_word():
     return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 # start the development server using the run() method
 if __name__ == '__main__':
